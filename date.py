44d72048dfcd8e044d7aefa661898f577fb2907f
Cyril0563 <103357709+Cyril0563@users.noreply.github.com>
2022-10-14 13:44:59 +0800

    Add files via upload

---
+++ date.py
@@ -0,0 +1,74 @@
+# encoding: utf-8
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+import datetime
+from datetime import datetime
+import requests
+import time
+import random
+
+user_agent_list = [ \
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1" \
+        "Mozilla/5.0 (X11; CrOS i686 2268.111.0) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11", \
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1092.0 Safari/536.6", \
+        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1090.0 Safari/536.6", \
+        "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/19.77.34.5 Safari/537.1", \
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.9 Safari/536.5", \
+        "Mozilla/5.0 (Windows NT 6.0) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.36 Safari/536.5", \
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3", \
+        "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3", \
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_0) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3", \
+        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3", \
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3", \
+        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3", \
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3", \
+        "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3", \
+        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.0 Safari/536.3", \
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24", \
+        "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24"
+    ]
+    
+url = 'https://api.ooomn.com/api/holiday?date='
+
+
+with open('/date.txt', 'r') as f:
+    for d in f:
+        d = d.strip()
+        
+       
+        headers = {'User-Agent': random.choice(user_agent_list)}
+        
+        params = {
+            'date': d
+        }
+        
+        response = requests.get(url, headers=headers, params=params)
+        
+        data = response.json()
+        
+        data = data[0]
+        d = datetime.strptime(d, '%Y%m%d')
+        zj = d.isoweekday()
+        data_name = data['describe']
+        data_name_new = data_name['Name']
+        if zj == 1:
+            zj = '星期一'
+        elif zj == 2:
+            zj = '星期二'
+        elif zj == 3:
+            zj = '星期三'
+        elif zj == 4:
+            zj = '星期四'
+        elif zj == 5:
+            zj = '星期五'
+        elif zj == 6:
+            zj = '星期六'
+        elif zj == 7:
+            zj = '星期日'
+       
+        
+        with open('data_out.csv', 'a') as f:
+            f.write(data['date'] + ',' + data['msg'] + ',' + data_name_new + ',' + zj + '' + '\n')
+        time.sleep(1)
+
